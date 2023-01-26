80470afbc70a7a61990cda152919f25717230c29
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-01-25 23:33:27 -0800

    added function for instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -3,6 +3,23 @@ import React from 'react';
 
 export default function Instructions() {
 
+  function startGame() {
+    let instructions = document.querySelector(".instructions");
+    instructions.style.display = "none";
+
+    // if (!gameStatus) {
+    //   let tempArray = [1, 2, 3, 4]
+    //   // console.log(tempArray[Math.floor(Math.random() * 4)])
+    //   dropBall(tempArray[Math.floor(Math.random() * 4)]);
+    // }
+  }
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      startGame();
+    }
+  })
+
   return (
     <div className='instructions'> 
         <h2> 

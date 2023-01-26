abfe59aebaf62e078d7cf8194020d867ee509890
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-01-25 23:38:07 -0800

    added function to make paddle placeholder move side to side

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 
 export default function Paddle() {
+  let paddle = document.querySelector(".paddle");
+  let xCord = 700;
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight" || event.keyCode === 68) {
+      xCord += 15;
+      paddle.style.left = `${xCord}px`;
+    }
+  
+    if (event.key === "ArrowLeft" || event.keyCode === 65) {
+      xCord -= 15;
+      paddle.style.left = `${xCord}px`;
+    }
+  })
 
   return (
     <div className='paddle'> </div>

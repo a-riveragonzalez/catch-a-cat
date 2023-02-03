fdabb79e0b4d35dec0e1f971d4005aba7ff293b0
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-02-02 21:10:02 -0800

    added test function for dropping ball

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,13 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 export default function Ball() {
   // todo add function to make ball fall
+  // test function for making ball fall
+  // function dropBall(num) {
+  //   let yCord = 5;
 
+  //   setInterval(() => {
+  //     if (!gameStatus) {
+  //       yCord += 5;
+  //       ball4.style.top = `${yCord}px`;
+  //       if (yCord > 250) {
+  //         yCord = 5;
+  //       }
+  //     }
+  //   }, 100);
+  // }
   // todo make function to make balls fall at different times
 
-  return (
-    <div className='ball'>
-    </div>
-  );
+  return <div className="ball"></div>;
 }
